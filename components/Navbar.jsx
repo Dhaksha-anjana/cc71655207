17c7c56d1de35760c3e401c6da2e105ff645bcb7
Dhaksha-anjana <mugepriy1971_bai27@mepcoeng.ac.in>
2025-06-17 03:59:55 +0530

    second commit

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -11,6 +11,7 @@ export default function Navbar() {
         <li><Link to="/company">Company</Link></li>
         <li><Link to="/reports">Reports</Link></li>
       </ul>
+      <div className ="flex"></div>
     </nav>
   )
 }
